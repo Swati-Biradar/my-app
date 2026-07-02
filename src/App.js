e787d251dd36113561b312f1854f5bf00c4ebfe5
Swati-Biradar <biradarswatisb002@gmail.com>
2026-07-02 12:07:08 +0530

    Update Propeller logo and link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        https://cdn-ikponof.nitrocdn.com/vGqfYAGlOLDkYkJqZhYIYKEsibdbZnkc/assets/images/optimized/rev-f684a87/www.propelleraero.com/wp-content/uploads/2023/05/footer-logo.svg
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  className="App-link"
+  href="https://www.propelleraero.com/dirtmate/"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Visit DirtMate
+</a>
       </header>
     </div>
   );
